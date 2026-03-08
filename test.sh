31931a836d2e51bbd2d39e3015086be89d34f92e
Sanchari hazra <shazra@ucdavis.edu>
2026-03-08 14:55:15 -0700

    Update test script to include descriptions and comments

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 #run code and save oytput file
 ./search_files.sh project .txt "TODO" > test_output.txt
 
-#expected  output:
+#expected  output
 
 cat << EOF > expected.txt
 === Match Counts ===
@@ -21,30 +21,36 @@ Files with matches: 2
 Total matching lines: 3
 EOF
 
+#TEST 1: Comparing output to expected output when searching for TODO pattern
+
+echo "Test 1: Verifing correct TODO matches in the .txt files"
 if diff test_output.txt  expected.txt > /dev/null
 then
-	echo "Test 1: PASS"
+	echo "Test 1: PASS (Output matches expected results)"
 else
-	echo "Test 1: FAIL"
+	echo "Test 1: FAIL (Output does not match expected results)"
 fi
 
-#if the pattern doesn't exist (Should print fail)
+
+#TEST 2: Verifying script when pattern is not found
 ./search_files.sh project .txt "XYZ" > test_output2.txt
 
 if grep -q "Files with matches: 0" test_output2.txt
 then
-    echo "Test 2: PASS"
+    echo "Test 2: PASS (Script correctly reports no matches when pattern is not found)"
 else
-    echo "Test 2: FAIL"
+    echo "Test 2: FAIL (Script incorrectly reports matches when pattern is not present)"
 fi
 
-#is directory doesnt exist
-./search_files.sh invalid_directory .txt "TODO" > test_output3.txt 2>&1
+
+#TEST 3:  Verifying script directory does not exist
+
+./search_files.sh homework_directory .txt "TODO" > test_output3.txt 2>&1
 
 if grep -q "Error" test_output3.txt
 then
-    echo "Test 3: PASS"
+    echo "Test 3: PASS (Script correctly detects invalid directory)"
 else
-    echo "Test 3: FAIL"
+    echo "Test 3: FAIL (Script does not report an error for invalid directory)"
 fi
 
