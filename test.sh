ff3566b21e16f2a1c1bbc8228646473e48be30f3
Sanchari hazra <shazra@ucdavis.edu>
2026-03-07 17:21:25 -0800

    updated codes on test.sh

--- test.sh
+++ test.sh
@@ -7,14 +7,14 @@
 
 cat << EOF > expected.txt
 === Match Counts ===
-notes.txt: 2
 log.txt: 1
+notes.txt: 2
 === Matching Lines ===
---- notes.txt ---
+---log.txt ---
+TODO: check output
+---notes.txt ---
 TODO: fix parser
 TODO: add tests
---- log.txt ---
-TODO: check output
 === Summary ===
 Files scanned: 3
 Files with matches: 2
@@ -28,5 +28,23 @@ else
 	echo "Test 1: FAIL"
 fi
 
-#if 
+#if the pattern doesn't exist (Should print fail)
+./search_files.sh project .txt "XYZ" > test_output2.txt
+
+if grep -q "Files with matches: 0" test_output2.txt
+then
+    echo "Test 2: PASS"
+else
+    echo "Test 2: FAIL"
+fi
+
+#is directory doesnt exist
+./search_files.sh invalid_directory .txt "TODO" > test_output3.txt 2>&1
+
+if grep -q "Error" test_output3.txt
+then
+    echo "Test 3: PASS"
+else
+    echo "Test 3: FAIL"
+fi
 
